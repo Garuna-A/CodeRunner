0df5dd061658f8b59a7bf205462382dac04d237f
Garuna-A <mishra.anuragdli@gmail.com>
2026-06-04 11:25:16 +0530

    Add TLE error for infinite loops

--- compiler-backend/executeCpp.js
+++ compiler-backend/executeCpp.js
@@ -14,10 +14,15 @@ const executeCpp=(filepath)=>{
     const filename = `${jobId}.cpp`;
 
     return new Promise((resolve,reject)=>{
-        const command = `docker run --rm -v "${path.dirname(filepath)}":/app cpp-runner sh -c "g++ /app/${filename} -o /app/${jobId}.out && /app/${jobId}.out"`;
-
-        exec(command,(error,stdout,stderr)=>{
+        const hostPath = path.dirname(filepath).replace(/\\/g, '/');
+        const command = `docker run --rm -v ${hostPath}:/app cpp-runner sh -c "g++ /app/${filename} -o /app/${jobId}.out && /app/${jobId}.out"`;
+       
+        exec(command,{timeout:6000},(error,stdout,stderr)=>{
+            console.log("error:", error);
+            console.log("stdout:", stdout);
+            console.log("stderr",stderr);
             if(error){
+                if(error.killed)return resolve("TLE:Time Limit Exceeded")
                 return resolve(stderr||error.message);
             }
             if(stderr){
